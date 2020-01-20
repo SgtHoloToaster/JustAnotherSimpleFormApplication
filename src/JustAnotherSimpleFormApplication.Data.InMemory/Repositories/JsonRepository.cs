2bbf8ef0e2b81c4e1d8bbb832e19b1ef9b58a857
Mikhail Podolskiy <edge769@yandex.ru>
2020-01-20 18:38:48 +0300

    add InMemory JsonRepository

---
+++ src/JustAnotherSimpleFormApplication.Data.InMemory/Repositories/JsonRepository.cs
@@ -0,0 +1,19 @@
+﻿using JustAnotherSimpleFormApplication.Data.Interface.Models;
+using JustAnotherSimpleFormApplication.Data.Interface.Repositories;
+using Newtonsoft.Json.Linq;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace JustAnotherSimpleFormApplication.Data.InMemory.Repositories
+{
+    class JsonRepository : IJsonRepository
+    {
+        readonly static ConcurrentBag<JObject> _data = new ConcurrentBag<JObject>();
+
+        public void Add(JObject form) =>
+            _data.Add(form);
+
+        public IEnumerable<JObject> GetList(Query<JObject> query) =>
+            query.Apply(_data);
+    }
+}
---
+++ src/JustAnotherSimpleFormApplication.Data.Interface/Repositories/IJsonRepository.cs
@@ -0,0 +1,13 @@
+﻿using JustAnotherSimpleFormApplication.Data.Interface.Models;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace JustAnotherSimpleFormApplication.Data.Interface.Repositories
+{
+    public interface IJsonRepository
+    {
+        void Add(JObject form);
+
+        IEnumerable<JObject> GetList(Query<JObject> query);
+    }
+}
