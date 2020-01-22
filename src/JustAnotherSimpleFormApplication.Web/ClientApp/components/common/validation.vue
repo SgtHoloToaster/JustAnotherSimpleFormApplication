98896d7c8bf4bc1f90c0cb1ef5dd14c5b8ede3f9
Mikhail Podolskiy <edge769@yandex.ru>
2020-01-22 10:47:41 +0300

    add validation error component

---
+++ src/JustAnotherSimpleFormApplication.Web/ClientApp/components/common/validation.vue
@@ -0,0 +1,20 @@
+﻿<template>
+    <span style="display:grid;">
+        <small v-for="error in validator.getErrors(fieldName)" class="validatior-error-text">{{error}}</small>
+    </span>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            validator: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
