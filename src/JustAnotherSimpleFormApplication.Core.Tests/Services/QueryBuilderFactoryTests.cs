a4bc37c7355bccf17e2633ed0a7dd1615575376f
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-02 15:28:52 +0300

    add unit tests for QueryBuilderFactory

---
+++ src/JustAnotherSimpleFormApplication.Core.Tests/Services/QueryBuilderFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using JustAnotherSimpleFormApplication.Core.Services;
+using JustAnotherSimpleFormApplication.Core.Services.Abstract;
+using Newtonsoft.Json.Linq;
+using System;
+using Xunit;
+
+namespace JustAnotherSimpleFormApplication.Core.Tests.Services
+{
+    public class QueryBuilderFactoryTests
+    {
+        [Fact]
+        public void CanCreateQueryBuilder() =>
+            TestCreateBuilder<object>(result => Assert.NotNull(result));
+
+        [Fact]
+        public void CreatedQueryBuilderHasCorrectType() =>
+            TestCreateBuilder<JObject>(result => Assert.True(result is QueryBuilder<JObject>));
+
+        private void TestCreateBuilder<T>(Action<IQueryBuilder<T>> assert)
+        {
+            // arrange
+            var target = new QueryBuilderFactory();
+
+            // act
+            var result = target.Create<T>();
+
+            // assert
+            assert(result);
+        }
+    }
+}
